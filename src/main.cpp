80a793e75acb695c106c3a9e7ce43f7c8e7ba6eb
eram <eram@eliass-mbp.home>
2025-07-03 09:21:19 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "GLFW/glfw3.h"
+
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
